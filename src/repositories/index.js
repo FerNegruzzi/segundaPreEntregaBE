158d29a2ab925ee441257a9a0f8ccd114c3f9ff7
unknown <you@example.com>
2023-08-22 22:38:40 -0300

    trabajo finalizado, errores en railway

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,8 +1,6 @@
-// const MessageRepository = require("./envirorment.repository");
-// const MessageAdapter = require('./factory')
+const MessageRepository = require("./envirorment.repository");
+const MessageAdapter = require('./factory')
 
-// const envirorment = new MessageRepository(new MessageAdapter())
+const envirorment = new MessageRepository(new MessageAdapter())
 
-// module.exports = envirorment 
-
-// ESTA COMENTADO PORQUE NO ME ANDA EN RAILWAY
+module.exports = envirorment 
