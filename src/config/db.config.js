826f1fd13eda2d9629e3ceeddc3a18a0cf9210d6
unknown <you@example.com>
2023-08-22 21:43:30 -0300

    install de nodemon para railway

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,6 +4,6 @@ module.exports = {
     dbAdmin: process.env.DB_ADMIN,
     dbPassword: process.env.DB_PASSWORD,
     dbHost: process.env.DB_HOST,
-    dbUrl: process.env.DB_URL
+    dbUrl: process.env.MONGO_URL
 }
 
